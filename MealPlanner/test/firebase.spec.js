97c421af43da3757dff89ff4f25c74f2ff2f1e25
julakanti <pjulakanti@wisc.edu>
2020-10-30 19:54:35 -0500

    added more tests and changed to by async compliant, only committing test file

--- MealPlanner/test/firebase.spec.js
+++ MealPlanner/test/firebase.spec.js
@@ -1,23 +1,45 @@
 const assert = require('assert');
 const firebase = require('../backend/firebase');
 
+const test = require("firebase-functions-test")({
+    projectId: "meal-planner-2692d";
+  });
+
 describe('firebase' , function ()  {
-    it('should return an array of matching doc data given a query', function() {
+    it('should return an array of matching doc data given a query', () => {
         const queries = {}; //FILL
-        let result = query_users(queries);
-        assert.strictEqual(result, "FILL");
+        //await then result
+        return query_users(queries).then(result => {
+            assert.strictEqual(result, "FILL");
+        });
     });
 
-    it('should return true on success', function() {
-        const user = "";
-        let result = add_users(user);
-        assert.strictEqual(result, true);
+    it('should return true on success', () => {
+        const user = "Fred";
+        return add_users(user).then(result => {
+            assert.strictEqual(result, true);
+        });
     });
 
-    it('should return true ons success', function(){
-        const user = ""
-        let result = update_users(user);
+    it('should return true ons success', () => {
+        const user = "Fred";
+        return update_users(user).then(result => {
         assert.strictEqual(result, true)
+        });
+    });
+
+    it('should return true on success', () => {
+        const user = "Ruby";
+        return add_users(user).then(result => {
+            assert.strictEqual(result, true);
+        });
+    });
+
+    it('should return true ons success', () => {
+        const user = "Ruby";
+        return update_users(user).then(result => {
+        assert.strictEqual(result, true)
+        });
     });
 
 });
