795ee134ff880e835d1c1bebc362a8b2bc7ea065
BrandtBuchda <bbuchda@wisc.edu>
2020-10-30 21:16:53 -0500

    Added testing script (Disabled)

---
+++ MealPlanner/testing/testing.spec.js
@@ -0,0 +1,53 @@
+/**
+ * !This file is for testing purposes only and should not be built with the application
+ * 
+ */
+
+/*
+const assert = require('assert');
+const firebase = require('../backend/firebase');
+import * as fb from '../backend/firebase';
+
+const test = require("firebase-functions-test")({
+    projectId: "meal-planner-2692d"
+});
+
+describe('firebase' , function ()  {
+    it('should return an array of matching doc data given a query', () => {
+        const queries = {}; //FILL
+        //await then result
+        return fb.query_users(queries).then(result => {
+            assert.strictEqual(result, "FILL");
+        });
+    });
+
+    it('should return true on success', () => {
+        const user = "Fred";
+        return fb.add_users(user).then(result => {
+            assert.strictEqual(result, true);
+        });
+    });
+
+    it('should return true ons success', () => {
+        const user = "Fred";
+        return fb.update_users(user).then(result => {
+        assert.strictEqual(result, true)
+        });
+    });
+
+    it('should return true on success', () => {
+        const user = "Ruby";
+        return fb.add_users(user).then(result => {
+            assert.strictEqual(result, true);
+        });
+    });
+
+    it('should return true ons success', () => {
+        const user = "Ruby";
+        return fb.update_users(user).then(result => {
+        assert.strictEqual(result, true)
+        });
+    });
+
+});
+*/
