cac3bb7811cfe07aa3ddfa948d6ff315292da3ed
Naveena <vishnaveena@gmail.com>
2020-11-12 14:04:49 -0600

    using Pranav's changes in my branch
    playing around with the code for back-end testing using pranav's changes

--- MealPlanner/test/controller.spec.js
+++ MealPlanner/test/controller.spec.js
@@ -1,6 +1,40 @@
-import * as fb from '../backend/firebase';
-import controller from '../backend/controller';
-import base64 from 'base-64';
+var assert = require("assert");
+const controller = require('../backend/controller');
 
+//user_login test
+describe("user_login", function() {
+    it("returns true if the login information is correct", async function() {
+      var username = "pjulakanti";
+      var password = "mockword";
+      let expected = await controller.user_login(username, password);
+      assert.equal(expected.success, true);
+    });
 
-const MY_PROJECT_ID = "meal-planner-2692d";
+    it("returns false if the login information is incorrect", async function() {
+        var username = "bad";
+        var password = "bad";
+        let expected = await controller.user_login(username, password);
+        assert.equal(expected.success, false);
+    });
+
+    it("returns false if the login information is invalid", async function() {
+        var username = "ba   d";
+        var password = "ba   d";
+        let expected = await controller.user_login(username, password);
+        assert.equal(expected.success, false);
+    });
+
+    it("returns false if the login information finds multiple users", async function() {
+        var username = "duplicate";
+        var password = "user";
+        let expected = await controller.user_login(username, password);
+        assert.equal(expected.success, false);
+    });
+
+    it("returns false if the login information fails", async function() {
+        var username = "fail";
+        var password = "fail";
+        let expected = await controller.user_login(username, password);
+        assert.equal(expected.success, false);
+    });
+  });
---
+++ MealPlanner/test/helper.spec.js
@@ -0,0 +1,71 @@
+var assert = require("assert");
+import {string_to_array} from '../backend/helper.js';
+const helper = require('../backend/helper');
+
+
+describe("string_to_array", function() {
+
+    it("returns an array with single element \"first\"", function() {
+      var test1 = ["first"];
+      
+      assert.equal(helper.string_to_array("first"), test1[0]);
+    });
+  
+    it("returns an array with two elements [first, second]", function(){
+
+        let expected = helper.string_to_array("first,second"); //check that list is passed in without spaces
+        assert.equal(expected[1], "second");
+    });
+  
+  });
+
+describe("array_to_string", function(){
+
+    it("returns a string \"first\" when passing in array", function() {
+
+        var test2 = ["first"];
+        let expected = helper.array_to_string(test2)
+        assert.equal(test2[0], expected);
+    });
+});
+
+//makes sure no white space
+describe("check_string", function(){
+
+    it("returns true when passed an empty string", function() {
+
+        var str = "";
+        let expected = helper.check_string(str);
+        assert.equal(expected, true);
+    });
+    
+    it("returns true when passed an string with space", function() {
+
+        var str2 = "the hello";
+        let expected = helper.check_string(str2);
+        assert.equal(expected, true);
+    });
+    
+    it("returns false when passed an string without space", function() {
+
+        var str3 = "goodby";
+        let expected = helper.check_string(str3);
+        assert.equal(expected, false);
+    }); 
+});
+
+
+describe("shuffleArray", function(){
+
+    it("returns a shuffled array given an array", function() {
+
+        var shuffleArr = ["first", "second", "third"];
+        let expected = helper.shuffleArray(shuffleArr);
+        var index1 = shuffleArr.indexOf("first");
+        var index2 = shuffleArr.indexOf("second");
+        var index3 = shuffleArr.indexOf("third");
+        assert.notEqual(index1, -1);
+        assert.notEqual(index2, -1);
+        assert.notEqual(index3, -1);
+    });
+});
